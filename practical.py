3bd3927fba837615b435451489b1531cb23441db
rohandhadke <92531028+rohandhadke@users.noreply.github.com>
2024-05-09 10:03:59 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,4 +1,4 @@
 print("!!Hello This is Google Cloud App Engine Programm on GitHub Created By Rohan Dhadke!!")
 print("Operations performed in Python - ")
-print(1. Addition)
+print("1. Addition")
 print("10+10 = ",10+10)
